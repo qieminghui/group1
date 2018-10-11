efdd3d9989be04b95fd8d70eca0bc56a4fed083a
Author Name <Author Email>
2018-10-11 12:38:34 +0800

    æµè¯githubæäº¤ä»£ç 

--- blog/dburl.js
+++
@@ -1,4 +0,0 @@
-//用来保存数据库的链接地址
-module.exports = {
-	dburl:"mongodb://localhost:27017/1314blog"
-}
--- zhengjie/dbUrl.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by zjLUliuxiaoliu on 2017/3/17.
- */
-module.exports = {
-    dbUrl:"mongodb://localhost:27017/0317blog"
-}
