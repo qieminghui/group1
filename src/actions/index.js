6fe60a9d284e995a6ca9eada5818a1e3cceec7da
Author Name <Author Email>
2018-11-08 21:06:27 +0800

    update code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
-const VisibilityFilters = {
+/*
+ * action 类型
+ */
+const VisibilityFilter = {
     visibilityFilter: 'SHOW_ALL',
     todos: [
         {
@@ -11,4 +14,32 @@ const VisibilityFilters = {
         }
     ]
 }
-export {VisibilityFilters}
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+/*
+ * 其它的常量
+ */
+
+export const VisibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+/*
+ * action 创建函数
+ */
+
+export function addTodo(text) {
+    return { type: ADD_TODO, text }
+}
+
+export function toggleTodo(index) {
+    return { type: TOGGLE_TODO, index }
+}
+
+export function setVisibilityFilter(filter) {
+    return { type: SET_VISIBILITY_FILTER, filter }
+}
