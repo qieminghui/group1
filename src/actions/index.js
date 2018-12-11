da3249e346d0ff380734ca03922307af3841e1c6
郄明辉 <Author Email>
2018-12-11 21:03:43 +0800

    update code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,88 @@
-let nextTodoId = 0
-export const addTodo = text => {
+// let nextTodoId = 0;
+//
+// export const addTodo = text => {
+//     return {
+//         type: 'ADD_TODO',
+//         id: nextTodoId++,
+//         text
+//     }
+// }
+//
+// export const setVisibilityFilter = filter => {
+//     return {
+//         type: 'SET_VISIBILITY_FILTER',
+//         filter
+//     }
+// }
+//
+// export const toggleTodo = id => {
+//     return {
+//         type: 'TOGGLE_TODO',
+//         id
+//     }
+// };
+
+import fetch from 'cross-fetch'
+
+export const REQUEST_POSTS = 'REQUEST_POSTS'
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
+export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
+
+export function selectSubreddit(subreddit) {
     return {
-        type: 'ADD_TODO',
-        id: nextTodoId++,
-        text
+        type: SELECT_SUBREDDIT,
+        subreddit
     }
 }
 
-export const setVisibilityFilter = filter => {
+export function invalidateSubreddit(subreddit) {
     return {
-        type: 'SET_VISIBILITY_FILTER',
-        filter
+        type: INVALIDATE_SUBREDDIT,
+        subreddit
     }
 }
 
-export const toggleTodo = id => {
+function requestPosts(subreddit) {
     return {
-        type: 'TOGGLE_TODO',
-        id
+        type: REQUEST_POSTS,
+        subreddit
+    }
+}
+
+function receivePosts(subreddit, json) {
+    return {
+        type: RECEIVE_POSTS,
+        subreddit,
+        posts: json.data.children.map(child => child.data),
+        receivedAt: Date.now()
+    }
+}
+
+function fetchPosts(subreddit) {
+    return dispatch => {
+        dispatch(requestPosts(subreddit))
+        return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+            .then(response => response.json())
+            .then(json => dispatch(receivePosts(subreddit, json)))
+    }
+}
+
+function shouldFetchPosts(state, subreddit) {
+    const posts = state.postsBySubreddit[subreddit]
+    if (!posts) {
+        return true
+    } else if (posts.isFetching) {
+        return false
+    } else {
+        return posts.didInvalidate
+    }
+}
+
+export function fetchPostsIfNeeded(subreddit) {
+    return (dispatch, getState) => {
+        if (shouldFetchPosts(getState(), subreddit)) {
+            return dispatch(fetchPosts(subreddit))
+        }
     }
 }
